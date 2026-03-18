34fdcfc9f643b3a6477beaeaa6226f8b67b4c596
samarth1299 <49311881+samarth1299@users.noreply.github.com>
2026-03-18 19:08:18 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import SalesPipelinePredictor from '@/components/SalesPipelinePredictor';
+
+export default function Home() {
+  return (
+    <main style={{ backgroundColor: '#f3f4f6', minHeight: '100vh' }}>
+      <SalesPipelinePredictor />
+    </main>
+  );
+}
